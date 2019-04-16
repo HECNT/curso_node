4c034f71ffa7a7b0c29452a8d4b5c99f7cfb1707
Osvaldo Hernandez <osvaldo@gabssa.net>
2019-04-16 18:23:14 -0500

    se creo repo

---
+++ app.js
@@ -0,0 +1,8 @@
+var libreria = require('./funcion')
+
+var args = process.argv.slice(2);
+console.log(args);
+
+var res = libreria.suma(args[0], args[1])
+
+console.log(res);
---
+++ funcion.js
@@ -0,0 +1,7 @@
+module.exports.suma = function (a, b) {
+    return parseInt(a)+parseInt(b);
+}
+
+module.exports.resta = function (a, b) {
+    return a-b;
+}
